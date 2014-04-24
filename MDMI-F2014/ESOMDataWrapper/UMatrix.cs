21b19546834dcad3c3222de092888f21effc5afd
sdebitu <sdeb@itu.dk>
2014-04-24 11:03:35 +0200

    added data wrappers for the ESOM result format files

---
+++ MDMI-F2014/ESOMDataWrapper/UMatrix.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ESOMDataWrapper
+{
+    internal class UMatrix
+    {
+        public readonly double[,] Heights;
+
+        public UMatrix(double[,] heights)
+        {
+            Heights = heights;
+        }
+
+        public static UMatrix FromFile(string fileName)
+        {
+            try
+            {
+                using (var sr = new StreamReader(fileName))
+                {
+                    const string klRegexStr = @"\% ?([0-9]+ ?){2}$";
+                    var klRegex = new Regex(klRegexStr);
+                    var headerRead = false;
+                    var k = -1;
+                    var l = -1;
+                    while (!headerRead)
+                    {
+                        var line = sr.ReadLine();
+                        if (!klRegex.IsMatch(line)) continue;
+                        var kl = line.Replace("%", "").Split(new[] {' '});
+                        k = int.Parse(kl[0]);
+                        l = int.Parse(kl[1]);
+                        headerRead = true;
+                    }
+                    var heights = new double[k, l];
+                    var data = sr.ReadToEnd();
+                    var lines = data.Split(new[] {'\n'});
+                    var i = 0;
+                    var j = 0;
+                    foreach (var height in lines.Select(line => line.Split(new[] {'\t'})))
+                    {
+                        foreach (var h in height)
+                        {
+                            heights[i, j] = double.Parse(h);
+                        }
+                        j++;
+                        if (j == l) i++;
+                    }
+                    return new UMatrix(heights);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File could not be read");
+                Console.WriteLine(e.Message);
+            }
+            return null;
+        }
+    }
+}
---
+++ MDMI-F2014/ESOMDataWrapperTest/ESOMTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ESOMDataWrapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ESOMDataWrapperTest
+{		
+		
+		
+    [TestClass]
+    public class ESOMTests
+    {
+        [TestMethod]
+        public void TestReadESOMFromFile()
+        {
+            var esom = ESOM.Fromfile("testFile.wts");
+            var data = new double[1,3,3];
+            data[0, 0, 0] = -0.701;
+            data[0, 0, 1] = -0.487;
+            data[0, 0, 2] = 0.793;
+            data[0, 1, 0] = -0.435;
+            data[0, 1, 1] = -0.439;
+            data[0, 1, 2] = 1.01;
+            data[0, 2, 0] = -0.136;
+            data[0, 2, 1] = -0.314;
+            data[0, 2, 2] = 1.198;
+            var expected = new ESOM(data);
+            for (var i = 0; i < 1; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    for (var n = 0; n < 3; n++)
+                    {
+                        Assert.AreEqual(expected.GetSOM()[i,j,n],
+                            esom.GetSOM()[i,j,n]);
+                    }
+                }
+            }
+        }
+    }
+}
