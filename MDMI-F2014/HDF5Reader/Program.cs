680719dabdf849593db9d364f178650dfe8765db
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-04-26 17:57:15 +0200

    Added HDF5 stuff to lib

---
+++ MDMI-F2014/HDF5Reader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HDF5DotNet;
+
+namespace HDF5Reader
+{
+    class Program
+    {
+        private static string file_path = @"C:\Users\hypesystem\Dropbox\Public\programmering\MDMI-F2014\data_sample\TRAXLZU12903D05F94.h5";
+
+        static void Main(string[] args)
+        {
+            H5.Open();
+            var is_hdf5 = H5F.is_hdf5(file_path);
+            Console.WriteLine("Is hdf5? " + is_hdf5);
+            Console.ReadKey();
+            var file_id = H5F.open(file_path, H5F.OpenMode.ACC_RDONLY);
+        }
+    }
+}
