6c89dbb86bad0438771bf3c07f088853c877f92f
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-05-17 00:00:56 +0200

    Added reader

--- MDMI-F2014/LRNWriter/LRNReader.cs
+++ MDMI-F2014/LRNWriter/LRNReader.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using MillionSongsDataWrapper;
+using HDF5Reader;
 
 namespace LRNWriter
 {
     public class LRNReader
     {
+        private string _filepath;
+
+        public LRNReader(string filepath) {
+            _filepath = filepath;
+        }
+
+        IEnumerable<Song> ReadSongs()
+        {
+            using (var reader = new StreamReader(_filepath))
+            {
+                var rows = reader.ReadLine();
+                var cols = reader.ReadLine();
+                var types = reader.ReadLine();
+                var names = reader.ReadLine();
+
+                while (!reader.EndOfStream)
+                {
+                    var row = reader.ReadLine();
+                    yield return ParseRow(row);
+                }
+            }
+        }
+
+        Song ParseRow(string row_data)
+        {
+            var song_builder = new SongBuilder();
+            var fields = row_data.Split('\t');
+
+            throw new NotImplementedException();
+        }
     }
 }
